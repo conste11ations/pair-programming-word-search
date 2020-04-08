ab11a468329b32c041d4d7441a23f87099ff6ca4
Rachel Kat <rachelkat@gmail.com>
2020-04-08 22:02:45 +0000

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let verticalJoin;
+  let result = false;
+  for (let array = 0; array < horizontalJoin.length; array++) {
+    if (horizontalJoin[array].includes(word)) {
+      result = true;
     }
-}
+  }
+  for (let firstArray = 0; firstArray < letters.length; firstArray++) {
+    for (let secondArray = 0; secondArray < letters[firstArray].length; secondArray++) {
+      if (letters[secondArray][firstArray] !== undefined) {
+        verticalJoin += letters[secondArray][firstArray];
+        if (verticalJoin.includes(word)) {
+          result = true;
+        }
+      }
+    }
+  }
+  return result;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
